1e97c9bf77eb166aa55130473a9066a206398bee
Ankush4101 <ankushdesai4101@gmail.com>
2024-04-07 14:04:29 -0400

    refactor code

--- src/main/java/de/storchp/opentracks/osmplugin/MapsActivity.java
+++ src/main/java/de/storchp/opentracks/osmplugin/MapsActivity.java
@@ -382,7 +382,7 @@ public class MapsActivity extends BaseActivity implements ItemizedLayer.OnItemGe
         //getting speed for individual segment
         double speedForSegment = getSegmentSpeed(selectedSegmentInTrack);
         //converting segment speed in Km/Hr from m/s.
-        double segmentSpeedInKmPerHour = getSegmentSpeedInKmPerHour(speedForSegment);
+        double segmentSpeedInKmPerHour = getSpeedInKmPerHour(speedForSegment);
         //formatting segment speed to show in table.
         String formattedSegmentSpeedInKmPerHour = formatSpeed(segmentSpeedInKmPerHour);
 
@@ -405,7 +405,7 @@ public class MapsActivity extends BaseActivity implements ItemizedLayer.OnItemGe
         return trackToBePopulated.avgMovingSpeedMeterPerSecond();
     }
 
-    //converting average speed in Km/Hr from m/s.
+    //converting  speed in Km/Hr from m/s.
     private double getSpeedInKmPerHour(double speedInMeterPerSec){
         return (speedInMeterPerSec * 3.6);
     }
@@ -420,10 +420,8 @@ public class MapsActivity extends BaseActivity implements ItemizedLayer.OnItemGe
         return selectedSegmentOfTrack.getSpeed();
     }
 
-    //converting segment speed in Km/Hr from m/s.
-    private double getSegmentSpeedInKmPerHour(double speedInMeterPerSec){
-        return (speedInMeterPerSec * 3.6);
-    }
+
+
 
     private double getSlopePercentage(double distance, float elevation) {
         // Slope percentage is (elevation change / horizontal distance) * 100
